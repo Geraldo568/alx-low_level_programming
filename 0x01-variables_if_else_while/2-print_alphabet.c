a3500a888509c66f255ab19067ce401abbeed8e3
Geraldo568 <geraldoanato19@gmail.com>
2022-12-09 21:32:39 +0100

    I play what I call the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 /**
 *main-entry point
-*Return: wether the number is, greater than 5, less than 6 and not 0 or zero
+*Return: alphabet in lowercase
 */
 int main(void)
 {
 	int i;
-	for(i = 'a'; i <= 'z'; i ++) 
+for (i = 'a'; i <= 'z'; i++)
 	putchar(i);
-	printf("\n");
-	return(0);
+	putchar('\n');
+	return (0);
 }
