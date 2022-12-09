253372bcc29be0ed08fce0dbac33bfa5321cee7b
Geraldo568 <geraldoanato19@gmail.com>
2022-12-09 22:24:33 +0100

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main-entry point
+*Return:  lowercase alphabet in reverse
+*/
+int main(void)
+{
+int i;
+for (i = 'z'; i <= 'a' ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
