0b8dd7346e538b1cb7bd9d5b921b24b19029d265
Geraldo568 <geraldoanato19@gmail.com>
2022-12-09 21:07:52 +0100

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main-entry point
+*Return: wether the number is, greater than 5, less than 6 and not 0 or zero
+*/
+int main(void)
+{
+	int i;
+	for(i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	
+	return(0);
+}
